e6c4d8d81c067e68a7f9a9a2dde5b0557970d690
Bob <mike.baradaran@qa.com>
2023-05-15 19:40:21 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+
+namespace ConsoleApp11;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Save();
+        Load();
+    }
+
+    static void Load() {
+        string strData = File.ReadAllText("data.json");
+
+        var zoo = JsonSerializer.Deserialize<Zoo>(strData);
+        
+        Console.WriteLine( "\n" + zoo.zooName + "\t" + zoo.yearStarted);
+        
+        foreach (var item in zoo.animals) {
+            Console.WriteLine(item.ID + "\t" + item.Name);
+        }
+    }
+
+    static void Save() {
+        Zoo zoo = new Zoo();
+        //var options = new JsonSerializerOptions
+        //{
+        //    WriteIndented = true
+        //};
+        var strData = JsonSerializer.Serialize(zoo); //, options);
+        Console.WriteLine(strData);
+        File.WriteAllText("data.json", strData);
+    }
+}
---
+++ Zoo.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleApp11;
+
+public class Animal
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+
+    public Animal(int iD, string name)
+    {
+        ID = iD;
+        Name = name;
+    }
+        
+}
+public class Zoo
+{
+    public int yearStarted { get; set; }
+    public string zooName { get; set; }
+
+    private List<string> animalNames = new List<string>() { "Zebra", "Lion", "Buffalo" };
+
+    public List<Animal> animals { get; set; }
+
+    public Zoo()
+    {
+        animals = new List<Animal>();
+        int id = 100;
+        foreach (var name in animalNames)
+        {
+            animals.Add(new Animal(id++, name));
+        }
+        zooName = "QA Zoo";
+        yearStarted = 1985;
+    }
+}
